f969b02e4d17c9c03ad88323d27bab5cadebfa03
Krešimir <kresimir.budor@gmail.com>
2017-03-31 11:50:00 +0200

    Cvita

--- AgroCore/AgroCore/Controllers/HomeController.cs
+++ AgroCore/AgroCore/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace AgroCore.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "SVITA SCITA SVITA";
 
             return View();
         }
